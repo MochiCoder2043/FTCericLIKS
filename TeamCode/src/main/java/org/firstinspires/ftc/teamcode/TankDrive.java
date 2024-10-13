b8cac9c268e0971ffc48293c7bcbc55de391094b
Mochi Dpp <mochidpp@192.168.1.65>
2024-10-13 16:24:30 -0600

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -30,24 +30,32 @@ public class TankDrive extends OpMode {
     //test
     @Override
     public void loop() {
+        // declarando la variable para la velocidad del movimiento hacia enfrente
         double wheelPower = -gamepad1.left_stick_y;
+
+        // declarando la variable para la velocidad del movimiento cangrejo
         double slidePower = -gamepad1.left_stick_x;
 
+        // checando si esta precionado el boton de la garra
         if (gamepad1.a){
-            claw.setPower(9.0);
+            // checkando si es para arriba el movimient (boton a)
+            claw.setPower(8.0);
         } else if (gamepad1.y) {
-            claw.setPower(-9.0);
+            // checkando si es para abajo el movimient (boton b)
+            claw.setPower(-8.0);
         }
 
+        // checkando si el robot se mueve cangrejo o no
         if (slidePower == 0){
+            // ir hacia enfrente
             leftMotor.setPower(wheelPower);
             rightMotor.setPower(wheelPower);
         } else if (slidePower != 0) {
+            // CANGREJO
             leftMotor.setPower(slidePower);
             rightMotor.setPower(slidePower);
             frontRightMotor.setPower(-slidePower);
             frontLeftMotor.setPower(-slidePower);
         }
-
     }
 }
